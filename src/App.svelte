6bcfbecf6ec5c64f9d472e9ed913729c8ae0be18
Phil <8772256+phillmp@users.noreply.github.com>
2024-06-03 11:50:54 -0700

    removed available to work message

--- src/App.svelte
+++ src/App.svelte
@@ -15,7 +15,7 @@
     <div id="titletext" class="flex flex-col items-center gap-y-2 text-center py-4">
       <h1 class="text-3xl text-slate-800 font-medium text-center mb-1">Phil Morris-Parent <span class="text-sm block text-slate-600">(they/them)</span></h1>
       <h2 class="text-base text-slate-800 font-light">Product Management | UX Design | Leadership</h2>
-      <div class="text-sm px-3 py-1 my-1 rounded-2xl bg-yellow-200 text-slate-800 max-w-fit font-medium">Available to work!</div>
+      <!-- <div class="text-sm px-3 py-1 my-1 rounded-2xl bg-yellow-200 text-slate-800 max-w-fit font-medium">Available to work!</div> -->
     </div>
     <div class="flex flex-col gap-y-3 items-stretch self-stretch my-4">
       <LinkButton href="https://www.linkedin.com/in/phillippe-morris-parent-92997369/"> LinkedIn </LinkButton>
